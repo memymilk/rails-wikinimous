55f886b2a0edac6871b5bf6b408e4c3fbeea7be7
marina belikova <memymilk@gmail.com>
2023-02-21 17:02:18 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "articles", to: "articles#index"
   get "articles/:id", to: "articles#show", as: :article
+  get "articles/:id/edit", to: "articles#edit", as: :edit
+  delete "articles/:id", to: "articles#destroy", as: :delete
+  patch "articles/:id", to: "articles#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
